78c1d8b31989d61837baaed28b83b75ce0a7eb6d
mi6154tana <mi6154tana@g.kumamoto-nct.ac.jp>
2019-09-21 17:26:46 +0900

    test_sock_3B update

--- test_sock_3B.py
+++ test_sock_3B.py
@@ -1,41 +1,35 @@
-import socket #UDP送信
+import socket #UDP
 import time
 import struct
 from contextlib import closing
 import os
 
-#送信の設定
-host = 192.168.11.20
+
+host = "192.168.11.10"
 send_port = 60000
-#受信の設定
+
 recv_ip = ""
 recv_port = 60000
-#2つのsocketを設定
-socksend = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #送信
-sockrecv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #受信
-sockrecv.bind((recv_ip, recv_port)) #socketを登録
+
+socksend = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sockrecv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sockrecv.bind((recv_ip, recv_port))
 
 
 with closing(socksend), closing(sockrecv):
 	
-	try:
-		while True:
-
-			#受信
-			print("Waiting for receive...")
-			data, addr = sockrecv.recvfrom(1024) #受信する dataに文字列が収納
-			#--受信するまでここで止まる--
-			num = struct.unpack('>i', data)[0] #data(バイト列)を整数型に変換
-			if num == 1:
-				#Raspi Zeroにデータ送信
-				#送信 dataのところは送信したい変数に書き換えてOK
-				socksend.sendto(num, (host, send_port))
-				break
-			elif num == 0:
-				#shutdown
-				os.system("sudo shutdown -h now")
-				break
-
-			
-    except KeyboardInterrupt:
-        pass
+        try:
+            while True:
+                print("Waiting for receive...")
+                data, addr = sockrecv.recvfrom(1024) 
+                num = struct.unpack('>i', data)[0]
+                if num == 1:
+                    send_num = struct.pack('>i', num)
+                    socksend.sendto(send_num, (host, send_port))
+                    break
+                elif num == 0:
+                    #shutdown
+                    os.system("sudo shutdown -h now")
+                    break
+        except KeyboardInterrupt:
+            pass
